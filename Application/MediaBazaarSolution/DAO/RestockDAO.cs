fc765d17ce027509cf75e0727327e6f65e6f8d88
Vaswani,Daniel D <d.vaswani@student.fontys.nl>
2020-05-30 14:04:13 +0200

    removed unneccesary reference

--- Application/MediaBazaarSolution/DAO/RestockDAO.cs
+++ Application/MediaBazaarSolution/DAO/RestockDAO.cs
@@ -1,11 +1,9 @@
-﻿using Org.BouncyCastle.Crypto.Tls;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Media.Converters;
 using MediaBazaarSolution.DTO;
 
 namespace MediaBazaarSolution.DAO
