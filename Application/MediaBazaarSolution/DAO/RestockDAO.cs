d5582b4a149ddae45c79838c262f6b28e690d1a7
Vaswani,Daniel D <d.vaswani@student.fontys.nl>
2020-05-30 14:01:00 +0200

    Created DTO and DAO Layers for Order tab (RestockDAO, Order, Alert)

---
+++ Application/MediaBazaarSolution/DAO/RestockDAO.cs
@@ -0,0 +1,78 @@
+﻿using Org.BouncyCastle.Crypto.Tls;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Converters;
+using MediaBazaarSolution.DTO;
+
+namespace MediaBazaarSolution.DAO
+{
+    public class RestockDAO
+    {
+        private static RestockDAO instance;
+
+        public static RestockDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new RestockDAO();
+                }
+
+                return RestockDAO.instance;
+            }
+
+            private set
+            {
+                RestockDAO.instance = value;
+            }
+        }
+            private RestockDAO() { }
+    
+        public List<Alert> LoadAlerts(bool sortAlertsByPriority)
+        {
+            string query = "SELECT * FROM alerts";
+            if (sortAlertsByPriority)
+                query += " ORDER BY priority";
+
+            List<Alert> alertList = new List<Alert>();
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+
+            foreach (DataRow row in data.Rows)
+            {
+                Alert alert = new Alert(row);
+                alertList.Add(alert);
+            }
+
+
+            return alertList;
+        }
+
+        public List<Order> LoadOrders(bool showCompletedOrders)
+        {
+            string query = "SELECT * FROM orders";
+            if(!showCompletedOrders)
+            query += "WHERE status = 'incomplete'";
+
+            List<Order> orderList = new List<Order>();
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+
+            foreach (DataRow row in data.Rows)
+            {
+                Order order = new Order(row);
+                orderList.Add(order);
+            }
+
+
+            return orderList;
+        }
+    }
+}
---
+++ Application/MediaBazaarSolution/DTO/Alert.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaarSolution.DTO
+{
+    public class Alert
+    {
+        private int iD;
+        private int stock;
+        private int limit;
+        private int priority;
+
+        public Alert(DataRow row)
+        {
+            this.iD = (int)row["item_id"];
+            this.stock = (int)row["stock"];
+            this.limit = (int)row["min_stock"];
+            this.priority = (int)row["priority"];
+        }
+
+        public Alert(int iD, int stock, int limit, int priority)
+        {
+            this.iD = iD;
+            this.stock = stock;
+            this.limit = limit;
+            this.priority = priority;
+        }
+
+        public int ID { get => this.iD; set => this.iD = value; }
+        public int Stock { get => this.stock; set => this.stock = value; }
+        public int Limit { get => this.limit; set => this.limit = value; }
+        public int Priority { get => this.priority; set => this.priority = value; }
+    }
+}
