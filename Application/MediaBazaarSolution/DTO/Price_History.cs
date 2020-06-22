aa56c1254dd2faee5370e58f6257af88eaa6d897
i430022 <i430022@fhict.nl>
2020-06-22 09:42:16 +0200

    Added Stock_History and Price_History DTO

---
+++ Application/MediaBazaarSolution/DTO/Price_History.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaarSolution.DTO
+{
+    class Price_History
+    {
+        
+        public Price_History(DataRow row)
+        {
+            this.ID = (int)row["item_id"];
+            this.amount = (int)row["price_change"];
+            this.date = (DateTime)row["timestamp"];
+        }
+
+        public int ID { get; private set; }
+        public int amount { get; private set; }
+        public DateTime date { get; private set; }
+    }
+}
